2d792924f99a7f32485992edcf49d699a49aaa7e
Mars <marianomarquis@gmail.com>
2016-01-17 12:57:20 -0600

    building

--- public/catphotoapp.php
+++ public/catphotoapp.php
@@ -3,8 +3,37 @@
   <head>
     <meta charset="utf-8">
     <title>Cat Photo App</title>
+    <link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+<style>
+  .red-text {
+    color: red;
+  }
+
+  h2 {
+    font-family: Lobster, Monospace;
+  }
+
+  p {
+    font-size: 16px;
+    font-family: Monospace;
+  }
+
+  .thick-green-border {
+    border-color: green;
+    border-width: 10px;
+    border-style: solid;
+    border-radius: 50%;
+  }
+
+  .smaller-image {
+    width: 100px;
+  }
+</style>
+
   </head>
   <body>
     <h2 class="red-text">CatPhotoApp</h2>
+    <p>Click her here for <a href="#">cat photos</a>.</p>
+    <a href="#"><img class="smaller-image thick-green-border" alt="A cute orange cat lying on its back"src="https://bit.ly/fcc-relaxing-cat"/></a>
   </body>
 </html>
