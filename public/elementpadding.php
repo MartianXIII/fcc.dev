924752086d4c9a82fe933557a555e2d1ac8db1ad
Mars <marianomarquis@gmail.com>
2016-01-17 19:47:35 -0600

    45 -

---
+++ public/elementpadding.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Adjusting Element Padding</title>
+    <style media="screen">
+
+    </style>
+  </head>
+  <body>
+    <h5 class="injected-text">margin</h5>
+  </body>
+</html>
