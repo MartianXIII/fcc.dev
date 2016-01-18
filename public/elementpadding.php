38acb8bcff33c6888365a96b41fa63ce5cabb88b
Mars <marianomarquis@gmail.com>
2016-01-17 19:53:14 -0600

    45 -

--- public/elementpadding.php
+++ public/elementpadding.php
@@ -4,7 +4,33 @@
     <meta charset="utf-8">
     <title>Adjusting Element Padding</title>
     <style media="screen">
+      .injected-text {
+        margin-bottom: -25px;
+        text-align: center;
+      }
 
+      .box {
+        border-style: solid;
+        border-color: black;
+        border-width: 5px;
+        text-align: center;
+      }
+
+      .yellow-box {
+        background-color: yellow;
+        padding: 10px;
+      }
+
+      .red-box {
+        background-color: red;
+        padding: 20px;
+      }
+
+      .green-box {
+        background-color: green;
+        padding: 10px;
+      }
+      
     </style>
   </head>
   <body>
