bde946a1b3f233adca4932e7676c593e88db2080
Mars <marianomarquis@gmail.com>
2016-01-28 11:07:32 -0600

    01-14

--- public/jqueryplayhouse.php
+++ public/jqueryplayhouse.php
@@ -7,6 +7,11 @@
   <body>
 
     <!-- Only change code above this line. -->
+    <script>
+      $(document).ready(function() {
+
+      });
+    </script>    
 
     <div class="container-fluid">
       <h3 class="text-primary text-center">jQuery Playground</h3>
