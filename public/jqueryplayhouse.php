ef30f1e196a2a9d0941c9665bb0f2e275992b5ef
Mars <marianomarquis@gmail.com>
2016-01-28 10:45:39 -0600

    01-02

--- public/jqueryplayhouse.php
+++ public/jqueryplayhouse.php
@@ -8,7 +8,7 @@
 
     <!-- Only change code above this line. -->
 
-    <!-- <div class="container-fluid"> -->
+    <div class="container-fluid">
       <h3 class="text-primary text-center">jQuery Playground</h3>
       <!-- <div class="row">
         <div class="col-xs-6">
@@ -27,8 +27,8 @@
             <button class="btn btn-default target" id="target6">#target6</button>
           </div>
         </div>
-      </div>
-    </div> -->
+      </div> -->
+    </div>
 
       <!--Jquery Playhouse  -->
 
