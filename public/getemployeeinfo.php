ca0c82c3c01bebb2806648cb0ffa3b8849512616
Mars <marianomarquis@gmail.com>
2016-03-02 12:35:01 -0600

    Define

--- public/getemployeeinfo.php
+++ public/getemployeeinfo.php
@@ -3,4 +3,7 @@
 // Connect to DB in Above Directory for security
 require_once('../mysql_connect.php');
 
+// Define our Query statement for Database
+$query = "SELECT emp_no, birth_date, first_name, last_name, gender, hire_date FROM employees";
+
 ?>
