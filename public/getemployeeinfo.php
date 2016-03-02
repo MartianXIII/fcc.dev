e1a80238f161c4c3d2dd2098eddea91881280541
Mars <marianomarquis@gmail.com>
2016-03-02 12:35:31 -0600

    Establish response

--- public/getemployeeinfo.php
+++ public/getemployeeinfo.php
@@ -6,4 +6,18 @@ require_once('../mysql_connect.php');
 // Define our Query statement for Database
 $query = "SELECT emp_no, birth_date, first_name, last_name, gender, hire_date FROM employees";
 
+// Establish response and pass database and prepaired query
+$response = @mysqli_query($dbc, $query);
+
+if($response)
+{
+  echo '<table
+  cellspacing="5" cellpadding="8">
+  <tr><td><b>Employee No<b></td>
+  <td><b>Birth Date<b></td>
+  <td><b>First Name<b></td>
+  <td><b>Last Name<b></td>
+  <td><b>Gender<b></td>
+  <td><b>Hire Date<b></td></tr>';
+
 ?>
